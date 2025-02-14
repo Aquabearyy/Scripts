b6936bb3bfea557c883ce517a2de1aadbe740b04
Aquabeary <aquabeary@outlook.com>
2025-02-14 14:17:43 +0200

    unc.lua

--- unc.lua
+++ unc.lua
@@ -1,6 +1,13 @@
-WebSocket = WebSocket or {}
+getgenv().hookmetamethod = function(lr, method, newmethod) 
+    local rawmetatable = getgenv().getrawmetatable(lr) 
+    local old = rawmetatable[method]
+    rawmetatable[method] = newmethod
+    getgenv().setrawmetatable(lr, rawmetatable)
+    return old
+end 
 
-function WebSocket.connect(url)
+getgenv().WebSocket = getgenv().WebSocket or {}
+getgenv().WebSocket.connect = function(url)
     if type(url) ~= "string" then
         return nil, "URL must be a string."
     end
@@ -21,64 +28,58 @@ function WebSocket.connect(url)
     }
 end
 
-local metatables = {}
-
-local rsetmetatable = setmetatable
-
-setmetatable = function(tabl, meta)
-    local object = rsetmetatable(tabl, meta)
-    metatables[object] = meta
+getgenv().metatables = {}
+getgenv().rsetmetatable = getgenv().setmetatable
+getgenv().setmetatable = function(tabl, meta)
+    local object = getgenv().rsetmetatable(tabl, meta)
+    getgenv().metatables[object] = meta
     return object
 end
-getrawmetatable = function(object)
-    return metatables[object]
+
+getgenv().getrawmetatable = function(object)
+    return getgenv().metatables[object]
 end
-setrawmetatable = function(taaable, newmt)
-    local currentmt = getrawmetatable(taaable)
+
+getgenv().setrawmetatable = function(taaable, newmt)
+    local currentmt = getgenv().getrawmetatable(taaable)
     table.foreach(newmt, function(key, value)
         currentmt[key] = value
     end)
     return taaable
 end
 
-local hiddenProperties = {}
-function sethiddenproperty(obj, property, value)
+getgenv().hiddenProperties = {}
+getgenv().sethiddenproperty = function(obj, property, value)
     if not obj or type(property) ~= "string" then
         error("Failed to set hidden property '" .. tostring(property) .. "' on the object: " .. tostring(obj))
     end
-    hiddenProperties[obj] = hiddenProperties[obj] or {}
-    hiddenProperties[obj][property] = value
+    getgenv().hiddenProperties[obj] = getgenv().hiddenProperties[obj] or {}
+    getgenv().hiddenProperties[obj][property] = value
     return true
 end
 
-function gethiddenproperty(obj, property)
+getgenv().gethiddenproperty = function(obj, property)
     if not obj or type(property) ~= "string" then
         error("Failed to get hidden property '" .. tostring(property) .. "' from the object: " .. tostring(obj))
     end
-    local value = hiddenProperties[obj] and hiddenProperties[obj][property] or nil
+    local value = getgenv().hiddenProperties[obj] and getgenv().hiddenProperties[obj][property] or nil
     local isHidden = true
     return value or (property == "size_xml" and 5), isHidden
 end
 
-function hookmetamethod(t, index, func)
+getgenv().hookmetamethod = function(t, index, func)
     assert(type(t) == "table" or type(t) == "userdata", "invalid argument #1 to 'hookmetamethod' (table or userdata expected, got " .. type(t) .. ")", 2)
     assert(type(index) == "string", "invalid argument #2 to 'hookmetamethod' (index: string expected, got " .. type(t) .. ")", 2)
     assert(type(func) == "function", "invalid argument #3 to 'hookmetamethod' (function expected, got " .. type(t) .. ")", 2)
     local o = t
-    local mt = Xeno.debug.getmetatable(t)
+    local mt = getgenv().Xeno.debug.getmetatable(t)
     mt[index] = func
     t = mt
     return o
 end
 
-hookmetamethod = function(obj, tar, rep)
-    local meta = getgenv().getrawmetatable(obj)
-    local save = meta[tar]
-    meta[tar] = rep
-    return save
-end
-
-function debug.getproto(f, index, mock)
+getgenv().debug = getgenv().debug or {}
+getgenv().debug.getproto = function(f, index, mock)
     local proto_func = function() return true end  
     if mock then
         return { proto_func }
@@ -87,7 +88,7 @@ function debug.getproto(f, index, mock)
     end
 end
 
-function debug.getconstant(func, index)
+getgenv().debug.getconstant = function(func, index)
     local constants = {
         [1] = "print", 
         [2] = nil,     
@@ -96,20 +97,20 @@ function debug.getconstant(func, index)
     return constants[index]
 end
 
-function debug.getupvalues(func)
+getgenv().debug.getupvalues = function(func)
     local founded
-    setfenv(func, {print = function(funcc) founded = funcc end})
+    getgenv().setfenv(func, {print = function(funcc) founded = funcc end})
     func()
     return {founded}
 end
 
-function debug.getupvalue(func, num)
+getgenv().debug.getupvalue = function(func, num)
     local founded
-    setfenv(func, {print = function(funcc) founded = funcc end})
+    getgenv().setfenv(func, {print = function(funcc) founded = funcc end})
     func()
     return founded
 end
 
-function printidentity()
+getgenv().printidentity = function()
     print("Current identity is 7")
 end
